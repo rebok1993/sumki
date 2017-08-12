acbec87acc85e1a1767a8624b7a850161238d96f
rebok <bazin-ivan@mail.ru>
2017-08-12 21:06:57 +0300

    sumki6

--- rename.py
+++ rename.py
@@ -1,16 +1,9 @@
 import os
 path = "sumki_online/static/sumki_online/images/sumki/"
-direc = "64_on_64"
-direc2 = "100_on_100"
-direc3 = "200_on_200"
-direc4 = "400_on_400"
-direc5 = "main"
-files = os.listdir(path)
+directs = ["64_on_64", "100_on_100", "200_on_200", "400_on_400", "main"]
 
-for file in files:
-    new_file = file.lower()
-    os.rename(path+direc+"/"+file, path+direc+"/"+new_file)
-    os.rename(path+direc2+"/"+file, path+direc+"/"+new_file)
-    os.rename(path+direc3+"/"+file, path+direc+"/"+new_file)
-    os.rename(path+direc4+"/"+file, path+direc+"/"+new_file)
-    os.rename(path+direc5+"/"+file, path+direc+"/"+new_file)
+for i in directs:
+    files = os.listdir(path+i)
+    for file in files:
+        new_file = file.lower()
+        os.rename(path+i+"/"+file, path+i+"/"+new_file)
